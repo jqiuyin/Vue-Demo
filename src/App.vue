78451262eeacf5f0c5a576a8412c97117cbf81bc
秋赟 季 <jiqy@shchinafortune.com>
2021-09-17 17:26:15 +0800

    lesson 53

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,84 @@
       山姆会员商店优惠商品
     </div>
     <div class="banner">
-      <img class="banner__img" src="http://www.dell-lee.com/imgs/vue3/banner.jpg"/>
+      <img
+        class="banner__img"
+        src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
+      />
     </div>
+    <div class="icons">
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/菜市场.png"
+        />
+        <p class="icons__item__desc">菜市场</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+    </div>
+  <div class="gap"></div>
   </div>
   <div class="docker">
     <div class="docker__item docker__item--active">
@@ -25,96 +101,118 @@
     <div class="docker__item">
       <div class="iconfont">&#xe61e;</div>
       <div class="docker__title">订单</div>
-      </div>
+    </div>
     <div class="docker__item">
       <div class="iconfont">&#xe72b;</div>
       <div class="docker__title">我的</div>
-      </div>
+    </div>
   </div>
-
 </template>
 
 <style lang="scss">
-@import './style/viriables.scss';
-@import './style/mixins.scss';
-.wrapper{
+@import "./style/viriables.scss";
+@import "./style/mixins.scss";
+.wrapper {
   position: absolute;
   left: 0;
   top: 0;
-  bottom: .5rem;
+  bottom: 0.5rem;
   right: 0;
-  padding: 0 .18rem;
+  padding: 0 0.18rem;
 }
-.position{
+.position {
   position: relative;
-  padding: .16rem .24rem .16rem 0;
-  line-height: .22rem;
-  line-height: .22rem;
-  font-size: .16rem;
+  padding: 0.16rem 0.24rem 0.16rem 0;
+  line-height: 0.22rem;
+  line-height: 0.22rem;
+  font-size: 0.16rem;
   @include ellipsis;
   color: $countent-fontcolor;
-  .position__icon{
+  .position__icon {
     position: relative;
-    top: .01rem;
-    font-size: .2rem;
+    top: 0.01rem;
+    font-size: 0.2rem;
   }
-  .position__notice{
+  .position__notice {
     position: absolute;
     right: 0;
-    top: .17rem;
-    font-size: .2rem;
+    top: 0.17rem;
+    font-size: 0.2rem;
   }
 }
-.search{
-  margin-bottom: .12rem;
-  line-height: .32rem;
-  background: #F5F5F5;
-  color: #B7B7B7;
-  border-radius: .16rem;
-  font-size: .14rem;
-  .iconfont{
+.search {
+  margin-bottom: 0.12rem;
+  line-height: 0.32rem;
+  background: #f5f5f5;
+  color: #b7b7b7;
+  border-radius: 0.16rem;
+  font-size: 0.14rem;
+  .iconfont {
     position: relative;
-    top: .04rem;
-    font-size: .2rem;
+    top: 0.04rem;
+    font-size: 0.2rem;
     display: inline-block;
-    padding: 0 .08rem 0 .16rem ;
+    padding: 0 0.08rem 0 0.16rem;
   }
 }
-.banner{
+.gap{
+  height: .1rem;
+  background: $centent-bgColor;
+  margin: 0 -.18rem;
+}
+.banner {
   height: 0;
   overflow: hidden;
   padding-bottom: 25.4%;
-  &__img{
+  &__img {
     width: 100%;
   }
 }
-.docker{
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: .16rem;
+  &__item {
+    width: 20%;
+    &__img{
+      display: block;
+      width: .4rem;
+      height: .4rem;
+      margin: 0 auto;
+    }
+    &__desc{
+      text-align: center;
+      margin: .06rem 0 .16rem 0;
+      color: $countent-fontcolor;
+    }
+  }
+}
+.docker {
   display: flex;
   box-sizing: border-box;
   position: absolute;
-  padding: 0 .18rem;
+  padding: 0 0.18rem;
   left: 0;
   bottom: 0;
   width: 100%;
-  height: .49rem;
-  border-top: 1px solid #F1F1F1;
+  height: 0.49rem;
+  border-top: 1px solid $centent-bgColor;
   color: $countent-fontcolor;
-  &__item{
+  &__item {
     flex: 1;
     text-align: center;
-    .iconfont{
-      margin: .07rem 0 .02rem 0;
-      font-size: .18rem;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
     }
-    &--active{
-      color: #1FA4FC;
+    &--active {
+      color: #1fa4fc;
     }
   }
-  &__title{
-    font-size: .2rem;
-    transform: scale(.5,.5);
+  &__title {
+    font-size: 0.2rem;
+    transform: scale(0.5, 0.5);
     transform-origin: center top;
   }
 }
-
 </style>
