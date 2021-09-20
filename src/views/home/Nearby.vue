1642022b7658f90e4b538cdee2008c3583cd4c4f
jqiuyin <943571574@qq.com>
2021-09-20 12:41:52 +0800

    lesson 55

---
+++ src/views/home/Nearby.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="nearby">
+    <h3 class="nearby_title">附近店铺</h3>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售一万加</span>
+          <span class="nearby__content__tag">月售一万加</span>
+          <span class="nearby__content__tag">月售一万加</span>
+        </div>
+        <p class="nearby__content__highlight">vip尊享</p>
+      </div>
+    </div>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售一万加</span>
+          <span class="nearby__content__tag">月售一万加</span>
+          <span class="nearby__content__tag">月售一万加</span>
+        </div>
+        <p class="nearby__content__highlight">vip尊享</p>
+      </div>
+    </div>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售一万加</span>
+          <span class="nearby__content__tag">月售一万加</span>
+          <span class="nearby__content__tag">月售一万加</span>
+        </div>
+        <p class="nearby__content__highlight">vip尊享</p>
+      </div>
+    </div>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售一万加</span>
+          <span class="nearby__content__tag">月售一万加</span>
+          <span class="nearby__content__tag">月售一万加</span>
+        </div>
+        <p class="nearby__content__highlight">vip尊享</p>
+      </div>
+    </div>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售一万加</span>
+          <span class="nearby__content__tag">月售一万加</span>
+          <span class="nearby__content__tag">月售一万加</span>
+        </div>
+        <p class="nearby__content__highlight">vip尊享</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Nearby'
+}
+</script>
+
+<style lang="scss">
+@import "../../style/viriables.scss";
+@import "../../style/mixins.scss";
+.nearby {
+  &__title {
+    margin: 0.16rem 0 0.02rem 0;
+    font-size: 0.18rem;
+    font-weight: normal;
+    color: $content-fontcolor;
+  }
+  &__item {
+    display: flex;
+    padding-top: 0.12rem;
+    &__img {
+      margin-right: 0.16rem;
+      width: 0.56rem;
+      height: 0.56rem;
+    }
+  }
+  &__content {
+    padding-bottom: 0.12rem;
+    border-bottom: 1px solid $content-bgColor;
+    flex: 1;
+    &__title {
+      line-height: 0.22rem;
+      font-size: 0.16rem;
+      color: $content-fontcolor;
+    }
+    &__tags {
+      margin-top: 0.08rem;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      color: $content-fontcolor;
+    }
+    &__tag {
+      margin-right: 0.16rem;
+    }
+    &__highlight {
+      color: #e93b3b;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      margin: 0.08rem 0 0 0;
+    }
+  }
+}
+</style>
---
+++ src/views/home/StaticPart.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="position">
+    <span class="iconfont position__icon">&#xe60e;</span>
+    北京理工大学
+    <span class="iconfont position__notice">&#xe7e5;</span>
+  </div>
+  <div class="search">
+    <span class="iconfont">&#xe6c6;</span>
+    山姆会员商店优惠商品
+  </div>
+  <div class="banner">
+    <img
+      class="banner__img"
+      src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
+    />
+  </div>
+  <div class="icons">
+    <div class="icons__item">
+      <img
+        class="icons__item__img"
+        src="http://www.dell-lee.com/imgs/vue3/超市.png"
+      />
+      <p class="icons__item__desc">超市便利</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__img"
+        src="http://www.dell-lee.com/imgs/vue3/菜市场.png"
+      />
+      <p class="icons__item__desc">菜市场</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__img"
+        src="http://www.dell-lee.com/imgs/vue3/水果店.png"
+      />
+      <p class="icons__item__desc">水果店</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__img"
+        src="http://www.dell-lee.com/imgs/vue3/鲜花.png"
+      />
+      <p class="icons__item__desc">鲜花绿植</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__img"
+        src="http://www.dell-lee.com/imgs/vue3/医药健康.png"
+      />
+      <p class="icons__item__desc">医药健康</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__img"
+        src="http://www.dell-lee.com/imgs/vue3/家居.png"
+      />
+      <p class="icons__item__desc">家居时尚</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__img"
+        src="http://www.dell-lee.com/imgs/vue3/蛋糕.png"
+      />
+      <p class="icons__item__desc">烘培蛋糕</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__img"
+        src="http://www.dell-lee.com/imgs/vue3/签到.png"
+      />
+      <p class="icons__item__desc">签到</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__img"
+        src="http://www.dell-lee.com/imgs/vue3/大牌免运.png"
+      />
+      <p class="icons__item__desc">大牌免运</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__img"
+        src="http://www.dell-lee.com/imgs/vue3/红包.png"
+      />
+      <p class="icons__item__desc">红包套餐</p>
+    </div>
+  </div>
+  <div class="gap"></div>
+</template>
+
+<script>
+export default {
+  name: 'StaticPart'
+}
+</script>
+
+<style lang="scss">
+@import "../../style/viriables.scss";
+@import "../../style/mixins.scss";
+.position {
+  position: relative;
+  padding: 0.16rem 0.24rem 0.16rem 0;
+  line-height: 0.22rem;
+  line-height: 0.22rem;
+  font-size: 0.16rem;
+  @include ellipsis;
+  color: $content-fontcolor;
+  .position__icon {
+    position: relative;
+    top: 0.01rem;
+    font-size: 0.2rem;
+  }
+  .position__notice {
+    position: absolute;
+    right: 0;
+    top: 0.17rem;
+    font-size: 0.2rem;
+  }
+}
+.search {
+  margin-bottom: 0.12rem;
+  line-height: 0.32rem;
+  background: #f5f5f5;
+  color: #b7b7b7;
+  border-radius: 0.16rem;
+  font-size: 0.14rem;
+  .iconfont {
+    position: relative;
+    top: 0.04rem;
+    font-size: 0.2rem;
+    display: inline-block;
+    padding: 0 0.08rem 0 0.16rem;
+  }
+}
+.gap {
+  height: 0.1rem;
+  background: $content-bgColor;
+  margin: 0 -0.18rem;
+}
+.banner {
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%;
+  &__img {
+    width: 100%;
+  }
+}
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 0.16rem;
+  &__item {
+    width: 20%;
+    &__img {
+      display: block;
+      width: 0.4rem;
+      height: 0.4rem;
+      margin: 0 auto;
+    }
+    &__desc {
+      text-align: center;
+      margin: 0.06rem 0 0.16rem 0;
+      color: $content-fontcolor;
+    }
+  }
+}
+</style>
