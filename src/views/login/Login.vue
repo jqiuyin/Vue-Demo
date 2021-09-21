e67c0f65fc2e1166a88a130b0fc273886e8da48b
jqiuyin <943571574@qq.com>
2021-09-21 16:07:43 +0800

    lesson 58

---
+++ src/views/login/Login.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="wrapper">
+    <img
+      class="wrapper__img"
+      src="http://www.dell-lee.com/imgs/vue3/user.png"
+    />
+    <div class="wrapper__input">
+      <input placeholder="请输入手机号" class="wrapper__input__content" />
+    </div>
+    <div class="wrapper__input">
+      <input placeholder="请输入密码" class="wrapper__input__content" />
+    </div>
+    <div class="wrapper__login-button">登录</div>
+    <div class="wrapper__login-link">立即注册</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/viriables.scss';
+.wrapper{
+  position: absolute;
+  top:50%;
+  left: 0;
+  right: 0;
+  transform: translateY(-50%);
+  &__img {
+    display: block;
+    margin: 0 auto .4rem auto;
+    width: .66rem;
+    height: .66rem;
+  }
+  &__input{
+    padding: 0 .16rem;
+    height: .48rem;
+    margin: 0 .4rem .16rem .4rem;
+    background: #F9F9F9;
+    border: 1px solid rgba(0, 0, 0, 0.10);
+    border-radius: 6px;
+    &__content{
+      border: none;
+      outline: none;
+      line-height: .48rem;
+      width: 100%;
+      background: none;
+      font-size: .16rem;
+      color: $content-notice-fontcolor;
+      &::placeholder{
+        color: $content-notice-fontcolor;
+      }
+    }
+  }
+  &__login-button{
+    margin: .32rem .4rem .16rem .4rem;
+    line-height: .48rem;
+    font-size: .16rem;
+    text-align: center;
+    background: #0091FF;
+    box-shadow: 0 .04rem .08rem 0 rgba(0,145,255,0.32);
+    border-radius: .04rem;
+    counter-reset: #FFF;
+  }
+  &__login-link{
+    font-size: .14rem;
+    color: $content-notice-fontcolor;
+    text-align: center;
+  }
+}
+</style>
