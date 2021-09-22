682339c7d10fb0c6f7f030041bc30d7174352474
秋赟 季 <jiqy@shchinafortune.com>
2021-09-22 17:39:35 +0800

    lesson 60

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/home/Home'
 import Login from '../views/login/Login'
+import Register from '../views/register/Register'
 const routes = [
   {
     path: '/',
@@ -11,6 +12,15 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+    beforeEnter (to, from, next) {
+      const { isLogin } = localStorage
+      isLogin ? next({ name: 'Home' }) : next()
+    }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
     beforeEnter (to, from, next) {
       const { isLogin } = localStorage
       isLogin ? next({ name: 'Home' }) : next()
@@ -32,8 +42,10 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  const { isLogin } = localStorage;
-  (isLogin || to.name === 'Login') ? next() : next({ name: 'Login' })
+  const { isLogin } = localStorage
+  const { name } = to
+  const isLoginOrRegister = (name === 'Login' || name === 'Register');
+  (isLogin || isLoginOrRegister) ? next() : next({ name: 'Login' })
 })
 
 export default router
--- src/views/register/Register.vue
+++ src/views/register/Register.vue
@@ -10,25 +10,24 @@
     <div class="wrapper__input">
       <input placeholder="请输入密码" type="password" class="wrapper__input__content" />
     </div>
-    <div class="wrapper__login-button" @click="handleLogin">登录</div>
-    <div class="wrapper__login-link" @click="handleRegisterClick">立即注册</div>
+    <div class="wrapper__input">
+      <input placeholder="确认密码" type="password" class="wrapper__input__content" />
+    </div>
+    <div class="wrapper__register-button">注册</div>
+    <div class="wrapper__register-link" @click="handleLoginClick">已有账号去登录</div>
   </div>
 </template>
 
 <script>
 import { useRouter } from 'vue-router'
 export default {
-  name: 'Login',
+  name: 'Register',
   setup () {
     const router = useRouter()
-    const handleLogin = () => {
-      localStorage.isLogin = true
-      router.push({ name: 'Home' })
+    const handleLoginClick = () => {
+      router.push({ name: 'Login' })
     }
-    const handleRegisterClick = () => {
-      router.push({ name: 'Register' })
-    }
-    return { handleLogin, handleRegisterClick }
+    return { handleLoginClick }
   }
 }
 </script>
@@ -67,7 +66,7 @@ export default {
       }
     }
   }
-  &__login-button{
+  &__register-button{
     margin: .32rem .4rem .16rem .4rem;
     line-height: .48rem;
     font-size: .16rem;
@@ -77,7 +76,7 @@ export default {
     border-radius: .04rem;
     counter-reset: #FFF;
   }
-  &__login-link{
+  &__register-link{
     font-size: .14rem;
     color: $content-notice-fontcolor;
     text-align: center;
