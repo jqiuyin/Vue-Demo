fa4e06fb95a7606d2e8d07f507caa443dcb5bf26
秋赟 季 <jiqy@shchinafortune.com>
2021-09-22 17:22:22 +0800

    lesson 59

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -10,14 +10,23 @@
     <div class="wrapper__input">
       <input placeholder="请输入密码" class="wrapper__input__content" />
     </div>
-    <div class="wrapper__login-button">登录</div>
+    <div class="wrapper__login-button" @click="handleLogin">登录</div>
     <div class="wrapper__login-link">立即注册</div>
   </div>
 </template>
 
 <script>
+import { useRouter } from 'vue-router'
 export default {
-  name: 'Login'
+  name: 'Login',
+  setup () {
+    const router = useRouter()
+    const handleLogin = () => {
+      localStorage.isLogin = true
+      router.push({ name: 'Home' })
+    }
+    return { handleLogin }
+  }
 }
 </script>
 
