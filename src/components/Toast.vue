fbd3be776f13fb3a2e648d27b8534015f087dd53
jqiuyin <943571574@qq.com>
2021-09-25 20:08:51 +0800

    lesson 63

---
+++ src/components/Toast.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="toast">{{message}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Toast',
+  props: ['message']
+}
+</script>
+
+<style lang="scss" scoped>
+.toast{
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    background: rgba(0,0,0,.35);
+    padding: .1rem;
+    border-radius: .05rem;
+    color: #FFF;
+}
+</style>
