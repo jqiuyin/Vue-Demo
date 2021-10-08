ab2494c6fc8680dc1fb93919731579f815c23913
秋赟 季 <jiqy@shchinafortune.com>
2021-10-08 18:50:21 +0800

    lesson 68

---
+++ src/components/ShopInfo.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="shop">
+      <img :src="item.imgUrl" class="shop__img"/>
+      <div class="shop__content">
+      <div class="shop__content__title">{{item.name}}</div>
+      <div class="shop__content__tags">
+          <span class="shop__content__tag">月售： {{item.sales}}</span>
+          <span class="shop__content__tag">起送： {{item.expressLimit}}</span>
+          <span class="shop__content__tag">基础运费： {{item.expressPrice}}</span>
+      </div>
+      <p class="shop__content__highlight">{{item.slogan}}</p>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ShopInfo',
+  props: ['item']
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/viriables.scss";
+.shop{
+  display: flex;
+  padding-top: 0.12rem;
+  &__img {
+      margin-right: 0.16rem;
+      width: 0.56rem;
+      height: 0.56rem;
+  }
+
+  &__content {
+    padding-bottom: 0.12rem;
+    border-bottom: 1px solid $content-bgColor;
+    flex: 1;
+    &__title {
+      line-height: 0.22rem;
+      font-size: 0.16rem;
+      color: $content-fontcolor;
+    }
+    &__tags {
+      margin-top: 0.08rem;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      color: $content-fontcolor;
+    }
+    &__tag {
+      margin-right: 0.16rem;
+    }
+    &__highlight {
+      color: #e93b3b;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      margin: 0.08rem 0 0 0;
+    }
+  }
+}
+</style>
--- src/views/home/Nearby.vue
+++ src/views/home/Nearby.vue
@@ -1,31 +1,14 @@
 <template>
   <div class="nearby">
     <h3 class="nearby_title">附近店铺</h3>
-    <div
-      class="nearby__item"
-      v-for="item in nearbyList"
-      :key="item._id"
-    >
-      <img
-        :src="item.imgUrl"
-        class="nearby__item__img"
-      />
-      <div class="nearby__content">
-        <div class="nearby__content__title">{{item.name}}</div>
-        <div class="nearby__content__tags">
-          <span class="nearby__content__tag">月售： {{item.sales}}</span>
-          <span class="nearby__content__tag">起送： {{item.expressLimit}}</span>
-          <span class="nearby__content__tag">基础运费： {{item.expressPrice}}</span>
-        </div>
-        <p class="nearby__content__highlight">{{item.slogan}}</p>
-      </div>
-    </div>
+    <ShopInfo v-for="item in nearbyList" :key="item._id" :item="item" />
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
 import { get } from '../../utils/request'
+import ShopInfo from '../../components/ShopInfo'
 
 const useNearbyListEffect = () => {
   const nearbyList = ref([])
@@ -41,6 +24,7 @@ const useNearbyListEffect = () => {
 
 export default {
   name: 'Nearby',
+  components: { ShopInfo },
   setup () {
     const { nearbyList, getNearbyList } = useNearbyListEffect()
     getNearbyList()
@@ -58,40 +42,6 @@ export default {
     font-size: 0.18rem;
     font-weight: normal;
     color: $content-fontcolor;
-  }
-  &__item {
-    display: flex;
-    padding-top: 0.12rem;
-    &__img {
-      margin-right: 0.16rem;
-      width: 0.56rem;
-      height: 0.56rem;
-    }
-  }
-  &__content {
-    padding-bottom: 0.12rem;
-    border-bottom: 1px solid $content-bgColor;
-    flex: 1;
-    &__title {
-      line-height: 0.22rem;
-      font-size: 0.16rem;
-      color: $content-fontcolor;
-    }
-    &__tags {
-      margin-top: 0.08rem;
-      line-height: 0.18rem;
-      font-size: 0.13rem;
-      color: $content-fontcolor;
-    }
-    &__tag {
-      margin-right: 0.16rem;
-    }
-    &__highlight {
-      color: #e93b3b;
-      line-height: 0.18rem;
-      font-size: 0.13rem;
-      margin: 0.08rem 0 0 0;
-    }
   }
 }
 </style>
