1a2be6c2fb28501de6bdccb3af7c9b4d0a86ffc9
秋赟 季 <jiqy@shchinafortune.com>
2021-10-12 18:17:44 +0800

    lesson 70

--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="wrapper">
+  <div class="search">
+    <div class="search__back iconfont">&#xe6db;</div>
+    <div class="search__content">
+      <span class="search__content__icon"></span>
+      <input class="search__content__input" />
+    </div>
+  </div>
     <ShopInfo :item="item" :hideBorder="true"/>
   </div>
 </template>
@@ -28,5 +35,36 @@ export default {
 <style lang="scss" scoped>
 .wrapper{
   padding: 0 .18rem;
+}
+.search{
+  height: .32rem;
+  display: flex;
+  margin: .2rem 0 .16rem 0;
+  line-height: .32rem;
+  &__back{
+    font-size: .24rem;
+    width: .3rem;
+    color: #B6B6B6;
+  }
+  &__content{
+    display: flex;
+    flex: 1;
+    background: #F5F5F5;
+    border-radius: .16rem;
+    &__icon{
+      width: .44rem;
+      height: .32rem;
+      background: blue;
+    }
+    &__input{
+      padding-right: .2rem;
+      width: 100%;
+      display: block;
+      height: .32rem;
+      border: none;
+      outline: none;
+      background: none;
+    }
+  }
 }
 </style>
