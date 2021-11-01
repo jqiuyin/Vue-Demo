755337c43e5bacf8e0b9996e441bdf0a8b295a5e
秋赟 季 <jiqy@shchinafortune.com>
2021-11-01 18:09:08 +0800

    lesson 83

--- src/views/shop/Cart.vue
+++ src/views/shop/Cart.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="cart">
+    <div class="product">
+      <div
+        class="product__item"
+        v-for="item in productList"
+        :key="item._id"
+      >
+        <img
+          class="product__item__img"
+          :src="item.imgUrl"
+        />
+        <div class="product__item__detail">
+          <h4 class="product__item__title">{{item.name}}</h4>
+          <p class="product__item__price">
+            <span class="product__item__yen">&yen;</span>{{item.price}}
+            <span class="product__item__origin">&yen;{{item.oldPrice}}</span>
+          </p>
+        </div>
+        <div class="product__number">
+          <span
+            class="product__number__minus"
+            @click="() => { changeCartItemInfo(shopId, item._id, item, -1) }"
+          >-</span>
+          {{ item.count || 0 }}
+          <span
+            class="product__number__plus"
+            @click="() => { changeCartItemInfo(shopId, item._id, item, 1) }"
+          >+</span>
+        </div>
+      </div>
+    </div>
       <div class="check">
         <div class="check__icon">
           <img
@@ -21,11 +51,9 @@ import { computed } from 'vue'
 import { useStore } from 'vuex'
 import { useRoute } from 'vue-router'
 
-const useCartEffect = () => {
+const useCartEffect = (shopId) => {
   const store = useStore()
-  const route = useRoute()
   const cartList = store.state.cartList
-  const shopId = route.params.id
   const total = computed(() => {
     const productList = cartList[shopId]
     let count = 0
@@ -48,20 +76,27 @@ const useCartEffect = () => {
     }
     return count.toFixed(2)
   })
-  return { total, price }
+  const productList = computed(() => {
+    const productList = cartList[shopId] || []
+    return productList
+  })
+  return { total, price, productList }
 }
 
 export default {
   name: 'Cart',
   setup () {
-    const { total, price } = useCartEffect()
-    return { total, price }
+    const route = useRoute()
+    const shopId = route.params.id
+    const { total, price, productList } = useCartEffect(shopId)
+    return { total, price, productList }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../../style/viriables.scss';
+@import '../../style/mixins.scss';
 .cart{
   position: absolute;
   left: 0;
@@ -115,6 +150,75 @@ export default {
     color: $bgcolor;
     font-size: .14rem;
     text-align: center;
+  }
+}
+
+.product {
+  flex: 1;
+  overflow-y: scroll;
+  background: #FFF;
+  &__item {
+    position: relative;
+    display: flex;
+    padding: 0.12rem 0;
+    margin: 0 0.16rem;
+    border-bottom: 0.01rem solid $content-bgColor;
+    &__detail{
+      overflow: hidden;
+    }
+    &__img {
+      width: 0.46rem;
+      height: 0.46rem;
+      margin-right: 0.16rem;
+    }
+    &__title {
+      margin: .06rem 0 0 0;
+      line-height: 0.2rem;
+      font-size: 0.14rem;
+      color: $content-fontcolor;
+      @include ellipsis;
+    }
+    &__price {
+      margin: 0;
+      line-height: 0.2rem;
+      font-size: 0.14rem;
+      color: $hightlight-fontColor;
+    }
+    &__yen {
+      font-size: 0.12rem;
+    }
+    &__origin {
+      line-height: 0.2rem;
+      font-size: 0.12rem;
+      color: $light-fontColor;
+      text-decoration: line-through;
+      margin-right: 0.06rem;
+    }
+    .product__number {
+      position: absolute;
+      right: 0;
+      bottom: 0.12rem;
+      &__plus,
+      &__minus {
+        width: 0.2rem;
+        height: 0.2rem;
+        line-height: 0.16rem;
+        display: inline-block;
+        border-radius: 50%;
+        font-size: 0.2rem;
+        text-align: center;
+      }
+      &__minus {
+        border: 0.01rem solid $medium-fontColor;
+        color: $medium-fontColor;
+        margin-right: 0.05rem;
+      }
+      &__plus {
+        background: $btn-bgColor;
+        color: $bgcolor;
+        margin-left: 0.05rem;
+      }
+    }
   }
 }
 </style>
