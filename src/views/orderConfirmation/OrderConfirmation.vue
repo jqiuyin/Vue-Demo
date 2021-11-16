c81aea8418dfee7dd38422b8e3e2904149bd7a4d
秋赟 季 <jiqy@shchinafortune.com>
2021-11-16 17:25:17 +0800

    lesson 95

--- src/views/orderConfirmation/OrderConfirmation.vue
+++ src/views/orderConfirmation/OrderConfirmation.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="top">
-      <div class="top__header">
-        <div class="iconfont top__header__back">&#xe6db;</div>
-        确认订单
-      </div>
-      <div class="top_receiver">
-        <div class="top__receiver__title">收货地址</div>
-        <div class="top__receiver__address">北京理工大学国防科技源。。。</div>
-        <div class="top__receiver__info">
-          <span class="top__receiver__info__name">瑶妹（先生）</span>
-          <span class="top__receiver__info__phone">189110245562</span>
+  <div class="wrapper">
+      <div class="top">
+        <div class="top__header">
+          <div class="iconfont top__header__back">&#xe6db;</div>
+          确认订单
         </div>
-        <div class="iconfont top__receiver__enter">&#xe6db;</div>
+        <div class="top__receiver">
+          <div class="top__receiver__title">收货地址</div>
+          <div class="top__receiver__address">北京理工大学国防科技源。。。</div>
+          <div class="top__receiver__info">
+            <span class="top__receiver__info__name">瑶妹（先生）</span>
+            <span class="top__receiver__info__phone">189110245562</span>
+          </div>
+          <div class="iconfont top__receiver__enter">&#xe6db;</div>
+        </div>
       </div>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -23,14 +25,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.wrapper{
+  position: absolute;
+  left: 0;
+  right: 0;
+  top:0;
+  bottom: 0;
+  background-color: #EEE;
+}
 .top{
+  position: relative;
   height: 1.96rem;
   background-size: 100% 1.59rem;
   background: linear-gradient(0deg, rgba(0,145,255,0.00) 4%, #0091FF 50%);
   background-repeat: no-repeat;
   &__header{
     position: relative;
-    padding-top: .4rem;
+    padding-top: .26rem;
     line-height: .24rem;
     color: #FFF;
     text-align: center;
@@ -39,6 +50,44 @@ export default {
       position: absolute;
       font-size: .22rem;
       left: .18rem;
+    }
+  }
+  &__receiver{
+    position: absolute;
+    left: .18rem;
+    right: .18rem;
+    bottom: 0;
+    height: 1.11rem;
+    background: #FFF;
+    border-radius: .04rem;
+    &__title{
+      padding: .16rem 0 .14rem .16rem;
+      font-size: .16rem;
+      color: #333;
+      line-height: .22rem;
+    }
+    &__address{
+      padding: 0 .4rem 0 .16rem;
+      font-size: .14rem;
+      color: #333;
+      line-height: .2rem;
+    }
+    &__info{
+      padding: .06rem 0 0 .16rem;
+      &__name{
+        font-size: .12rem;
+        color:#666;
+        line-height: .18rem;
+        margin-right: .06rem;
+      }
+    }
+    &__enter {
+      position: absolute;
+      right: .16rem;
+      top: .5rem;
+      color: #666;
+      font-size: .2rem;
+      transform: rotate(180deg);
     }
   }
 }
