db8d10bfad93481bcce51afb3544343010062f99
秋赟 季 <jiqy@shchinafortune.com>
2021-12-07 18:25:37 +0800

    lesson 106

--- src/views/orderList/OrderList.vue
+++ src/views/orderList/OrderList.vue
@@ -9,13 +9,14 @@
         </div>
         <div class="order__content">
           <div class="order__content__imgs">
-            <img class="order__content__img" src="" />
-            <img class="order__content__img" src="" />
-            <img class="order__content__img" src="" />
+            <img class="order__content__img" src="http://www.dell-lee.com/imgs/vue3/tomato.png" />
+            <img class="order__content__img" src="http://www.dell-lee.com/imgs/vue3/tomato.png" />
+            <img class="order__content__img" src="http://www.dell-lee.com/imgs/vue3/tomato.png" />
+            <img class="order__content__img" src="http://www.dell-lee.com/imgs/vue3/tomato.png" />
           </div>
-          <div class="order__info">
-            <div class="order__info__price">￥36.8</div>
-            <div class="order__info__count">共2件</div>
+          <div class="order__content__info">
+            <div class="order__content__price">￥36.8</div>
+            <div class="order__content__count">共2件</div>
           </div>
         </div>
       </div>
@@ -26,19 +27,41 @@
 
 <script>
 import Docker from '../../components/Docker'
+import { get } from '../../utils/request'
+import { reactive, toRefs } from 'vue'
+
+const useOrderListEffect = () => {
+  const data = reactive({ list: [] })
+  const getNearbyList = async () => {
+    const result = await get('/api/order')
+    console.log(result)
+    if (result?.errno === 0 && result?.data?.length) {
+      data.list = result.data
+    }
+  }
+  getNearbyList()
+  const { list } = toRefs(data)
+  return { list }
+}
 
 export default {
   name: 'OrderList',
-  components: { Docker }
+  components: { Docker },
+  setup () {
+    const list = useOrderListEffect()
+    return { list }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../../style/viriables.scss";
+@import "../../style/mixins.scss";
 .title{
   line-height: .44rem;
-  background: #FFF;
+  background: $bgcolor;
   font-size: .16rem;
-  color: #333;
+  color: $content-fontcolor;
   text-align: center;
 }
 .wrapper {
@@ -48,6 +71,50 @@ export default {
   top: 0;
   bottom: 0.5rem;
   right: 0;
-  background: #EEE;
+  background: rgb(248,248,248);
+}
+.order{
+  padding: .16rem;
+  background: $bgcolor;
+  margin: .16rem .18rem;
+  &__title{
+    font-size: .16rem;
+    color: $content-fontcolor;
+    line-height: .22rem;
+    margin-bottom: .16rem;
+  }
+  &__status{
+    float: right;
+    font-size: .14rem;
+    color: $light-fontColor;
+  }
+  &__content{
+    display: flex;
+    &__imgs{
+      flex: 1;
+
+    }
+    &__img{
+      width: .4rem;
+      height: .4rem;
+      margin-right: .12rem;
+    }
+    &__info{
+      width: .7rem;
+    }
+    &__price{
+      font-size: .14rem;
+      color: $hightlight-fontColor;
+      line-height: .2rem;
+      text-align: right;
+      margin-bottom: .04rem;
+    }
+    &__count{
+      font-size: .12rem;
+      color: $content-fontcolor;
+      text-align: right;
+      line-height: .14rem;
+    }
+  }
 }
 </style>
